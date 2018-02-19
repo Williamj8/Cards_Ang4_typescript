23418b27bbf1b383659cc053faa8c6aa942f9350
singhsdothings <singhrk0818@gmail.com>
2018-02-18 17:06:09 -0800

    Added Custom Pipe

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./Product-list.Component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Credit Cards';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
 
-    products: any[] = [
+    // Filtering a list
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+        // Added condtions to display nothing when empty input and 
+        //  Convert to lowercase if user types in Uppercase so it can match data.
+    }
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -34,8 +48,24 @@ export class ProductListComponent {
         },
 
     ];
+    // Set default value for filter
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'Cart' ;
 
-toggleImage(): void {
-    this.showImage = !this.showImage;   
-}
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
 }
