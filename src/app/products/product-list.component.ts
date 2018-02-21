5b7b29c47271dd81ab8552e86501e6828fbb9ef6
singhsdothings <singhrk0818@gmail.com>
2018-02-20 19:27:29 -0800

    Event Emitter

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -30,7 +30,7 @@ export class ProductListComponent implements OnInit {
             "productId": 1,
             "productName": "Gold Card",
             "productCode": "GDN-0011",
-            "releaseDate": "July 19, 2021",
+            "releaseDate": "July 09, 2021",
             "description": "Gold card with Membership",
             "price": 1000.99,
             "starRating": 3.2,
@@ -40,13 +40,42 @@ export class ProductListComponent implements OnInit {
             "productId": 2,
             "productName": "Blue Card",
             "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2022",
+            "releaseDate": "March 21, 2022",
             "description": "Blue card with corp membership",
             "price": 2500.99,
             "starRating": 4.5,
             "imageUrl": "https://openclipart.org/image/300px/svg_to_png/13200/Anonymous-smartcard.png"
         },
-
+        {
+            "productId": 3,
+            "productName": "Black Card",
+            "productCode": "GDN-0038",
+            "releaseDate": "Aug 18, 2028",
+            "description": "Black card with Membership",
+            "price": 8000.88,
+            "starRating": 5.0,
+            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/241542/bw-creditcard.png"
+        },
+        {
+            "productId": 4,
+            "productName": "Platinum Card",
+            "productCode": "GDN-0078",
+            "releaseDate": "Sep 19, 2025",
+            "description": "Platinum card with Membership",
+            "price": 5000.99,
+            "starRating": 4.2,
+            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/268431/bluecreditcard.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Brown Card",
+            "productCode": "GDN-0051",
+            "releaseDate": "June 05, 2020",
+            "description": "Brown card with Membership",
+            "price": 1000.99,
+            "starRating": 2.6,
+            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/217648/credit_card_path.png"
+        },
     ];
     // Set default value for filter
     constructor() {
